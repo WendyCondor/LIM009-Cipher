482b3ef4de3ea527ecad6510f30a85af40c76f45
WendyCondor <airamsaleth@gmail.com>
2019-02-25 09:07:54 -0500

    cambios en html y css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 /* Acá va tu código */
+var Cifrar = document.getElementById("text");
+var descifrar = document.getElementById("number");
+
+function codificar() {
+  var caracteres = [];
+  valor = cifrar.value;
+  for (var i=0; i <valor.length; i++) {
+    caracteres[i] = valor.charAt(i).charCodeAt(0);
+    }
+
+}
