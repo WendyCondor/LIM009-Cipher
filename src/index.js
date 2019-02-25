2fedf0d5c89d7bcd501e028d6eb02da5463455e4
WendyCondor <airamsaleth@gmail.com>
2019-02-25 12:41:11 -0500

    js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* Acá va tu código */
-var Cifrar = document.getElementById("text");
-var descifrar = document.getElementById("number");
+/* var descifrar = document.getElementById("number");
 
 function codificar() {
   var caracteres = [];
@@ -9,4 +8,12 @@ function codificar() {
     caracteres[i] = valor.charAt(i).charCodeAt(0);
     }
 
+let text = */
+var input= document.getElementById("Palabra a cifrar")
+var button= document.getElementById("cifrar")
+
+button.addEventListener("Click",function (){
+var text = input.value;
+const resultado = document.getElementById(id de donde va a botar el resultado);
+resultado.innerHTML = text;
 }
