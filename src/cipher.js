ba27cadbe6501829c2028602a4dca1e6effd42ab
WendyCondor <airamsaleth@gmail.com>
2019-02-27 23:23:09 -0500

    Cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,25 @@
 window.cipher = {
   encode: () => {
-    /* Acá va tu código */
-  },
+    /*const input = [];
+    const text = "";
+    let i;
+    for (i = 0; i < palbras.length; i++) {
+      text += palabras[i] + "<br>";
+    }
+    document.getElementById("Cifrar").innerHTML = text;
+
+     /* Acá va tu código */
+     const entrada= [];
+     let palabra= "";
+
+     for(let i = entrada.length -1; i >=0; i--){
+     palabra = palabra + entrada[i];
+     console.log(palabra);
+     },
   decode: () => {
     /* Acá va tu código */
   }
 };
+
+/*const cifrado = document.getElementById("text");
+const */
--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-/* Acá va tu código */
-/* var descifrar = document.getElementById("number");
-
-function codificar() {
-  var caracteres = [];
-  valor = cifrar.value;
-  for (var i=0; i <valor.length; i++) {
-    caracteres[i] = valor.charAt(i).charCodeAt(0);
-    }
-
-let text = */
+/* Para obtener id e imprimir en pantalla */
 const input= document.getElementById("text");
 const button= document.getElementById("cifrar");
 const text= document.getElementById("text1");
@@ -17,5 +7,16 @@ button.addEventListener("click", () => {
   const mensajeCifrado = input.value;
   console.log(mensajeCifrado);
   text.value = mensajeCifrado;
+
+});
+
+const input1= document.getElementById("text");
+const button1= document.getElementById("descifrar");
+const texto= document.getElementById("text1");
+
+button1.addEventListener("click", () => {
+  const mensajeDescifrado = input.value;
+  console.log(mensajeDescifrado);
+  texto.value = mensajeDescifrado;
 
 });
