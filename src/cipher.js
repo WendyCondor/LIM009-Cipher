aa8dc87d424b9df3031a8c79e3ea9d3455956ef6
WendyCondor <airamsaleth@gmail.com>
2019-03-04 01:01:00 -0500

    arreglos

--- src/cipher.js
+++ src/cipher.js
@@ -2,10 +2,10 @@ window.cipher = {
   encode: (text,number) => {
     let palabraCifrada = '';
     for( i =0; i <text.length; i++) {
-      if(text.chartCodeAt(i)== 32){// buscar que significa los 3 iguales, buscar else y
+      //if(text.chartCodeAt(i)== 32){// buscar que significa los 3 iguales, buscar else y
         //sino funciona crear un rango en el if, si es >65 y <90.
-        palabraCifrada='';
-      }
+        //palabraCifrada='';
+      //}
     const palabraAscii = text.charCodeAt(i);
     const formulaPalabra = (palabraAscii - 65 + parseInt(number))%26+65;
     const cifrado = String.fromCharCode(formulaPalabra);
