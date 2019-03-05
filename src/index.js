65cfb2c1e5b14f75da96b84b333d081eb64e2776
WendyCondor <airamsaleth@gmail.com>
2019-03-05 09:44:02 -0500

    Cambios Css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const button= document.getElementById("cifrar");
 const offset= document.getElementById("number");
 const text= document.getElementById("text1");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", (event) => {
+  event.preventDefault();
   const mensajeCifrado = input.value.toUpperCase(text);
   const desplazamiento = offset.value;
   text.value = cipher.encode(mensajeCifrado,desplazamiento);
@@ -14,9 +15,15 @@ button.addEventListener("click", () => {
 const button1= document.getElementById("descifrar");
 const texto= document.getElementById("text1");
 
-button1.addEventListener("click", () => {
+button1.addEventListener("click", (event) => {
+  event.preventDefault();
   const mensajeDescifrado = input.value.toUpperCase(text);
   const desplazamiento = offset.value;
   texto.value = cipher.decode (mensajeDescifrado,desplazamiento);
 
+});
+
+const buttonLimpiar= document.getElementById("refrescar");
+buttonLimpiar.addEventListener("click", () => {
+  document.getElementById("form").reset();
 });
