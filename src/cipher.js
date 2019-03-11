62e0c9714035e15f8038b9a46beb7ad52b4ec67f
WendyCondor <airamsaleth@gmail.com>
2019-03-10 21:43:45 -0500

    Cambios finales

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,20 @@
 window.cipher = {
+  //Explicacion del cifrado//
   encode: (text,number) => {
-    let palabraCifrada = '';
+    let palabraCifrada = ''; //variable que almacenará palabra cifrada//
+    //Bucle para recorrer cada letra de la palabra u oración a cifrar//
     for(let i =0; i <text.length; i++) {
-      //if(text.chartCodeAt(i)== 32){// buscar que significa los 3 iguales, buscar else y
-        //sino funciona crear un rango en el if, si es >65 y <90.
-        //palabraCifrada='';
-      //}
-    const palabraAscii = text.charCodeAt(i);
-    const formulaPalabra = (palabraAscii - 65 + parseInt(number))%26+65;
-    const cifrado = String.fromCharCode(formulaPalabra);
+    const palabraAscii = text.charCodeAt(i);//variable para obtener el código ASCII de cada letra//
+    const formulaPalabra = (palabraAscii - 65 + parseInt(number))%26+65;//Formula para obtener el código y para incluir el desplazamiento//
+    const cifrado = String.fromCharCode(formulaPalabra);//variable para convertir a la posición del alfabeto//
       palabraCifrada = palabraCifrada + cifrado;
     }
     return palabraCifrada
   },
+
+  //Explicacion del descifrado//
   decode: (text,number) => {
-    let palabraDescifrada = '';
+    let palabraDescifrada = ''; //variable que almacenará palabra descifrada//
     for ( let i=0; i<text.length; i++) {
     const palabraAscii2 = text.charCodeAt(i);
     const formulaPalabra2 = (palabraAscii2 + 65 - parseInt(number))%26 + 65;
